5a0c40fb529eb4f28b7fb6d82882d36d4173a4dd
blessykbabu <blessykbabu5@gmail.com>
2024-01-27 10:44:18 +0530

    commit

--- client/utils/url.js
+++ client/utils/url.js
@@ -1,6 +1,6 @@
 
  export default function urls(){
-    let HOSTED_SERVER_URL="http://18.222.140.6:5900";
+    let HOSTED_SERVER_URL="http://18.222.140.6:5700";
   //  let HOSTED_SERVER_URL="http://127.0.0.1:5000";
 
    let LOCAL_SERVER_URL="http://localhost:5000";
